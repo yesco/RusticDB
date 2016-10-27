90551ba5b24a8be2182d86c9ac5b1846935cd405
Jonas S Karlsson <jsk@yesco.org>
2016-10-26 23:18:51 -0700

    write storage to file, read the bytes but not "parse" yet.

--- rusticdb.rs
+++ rusticdb.rs
@@ -1,25 +1,8 @@
 fn main() {
   println!("---RusticDB---");
-  let s = "Hello World!";
-  println!("{:?}", s);
 
-  // --
-  for b in s.as_bytes() {
-    print!("{}, ", b);
-  }
+  
 
-  println!("");
-
-  println!("{:?}", s.as_bytes());
-
-  // --
-  for c in s.chars() {
-    print!("{}, ", c);
-  }
-
-  println!("");
-
-  println!("{:?}", s.chars());
 }
 
 
--- s.rs
+++ s.rs
@@ -1,3 +1,20 @@
+// Store functionality of RusticDB
+// (C) 2016-10-24 Jonas S Karlsson
+// 
+// Overall intentional design
+// - stores buckets of tuples
+// - a tuple has an bytes keyvalue, timestamp, deletemarker, bytes data
+// - a bucket is "one file"
+// - a bucket is mostly sorted
+// - new values are appended, in memory and on file
+// - sort order of vector should be retained, only deletemarker can be set
+// - can allow it to be GCed according to policy (TBD)
+
+use std::io::prelude::*;
+use std::io::Error;
+use std::fs::File;
+use std::mem::transmute;
+
 #[derive(Debug)]
 struct Tuple<'a> {
   key: &'a [u8],
@@ -9,6 +26,17 @@ struct Bucket<'a> {
   //element: &'a [&'a Tuple<'a>],
   vec: Vec<Tuple<'a>>,
 }
+
+// could use - http://stackoverflow.com/questions/29037033/how-to-slice-a-large-veci32-as-u8
+fn bytes(u: usize) -> [u8; 4] {
+  let i = u as i32;
+  return unsafe { transmute(i.to_be()) }; // or .to_le()
+}
+
+//fn i32(b: [u8; 4]) -> i32 {
+//  let i = u as i32;
+//  return unsafe { transmute(i.to_be()) }; // or .to_le()
+//}
 
 impl<'a> Bucket<'a> {
 
@@ -23,18 +51,50 @@ impl<'a> Bucket<'a> {
     println!("  {:?}", self);
   }
 
+  fn write(&self) -> Result<(), Error> {
+    println!("Write!");
+    let mut f = try!(File::create("bucket001.txt"));
+
+    for t in &self.vec {
+      try!(f.write_all(&bytes(t.key.len())));
+      try!(f.write_all(t.key));
+
+      try!(f.write_all(&bytes(t.data.len())));
+      try!(f.write_all(t.data));
+    }
+    try!(f.flush());
+
+    Ok(())
+  }
+
+  fn read(&self) -> Result<(), Error> {
+    println!("Read!");
+    let mut f = try!(File::open("bucket001.txt"));
+
+    // we read all data as one byte vector
+    // then we set up
+    let mut bytes = Vec::new();
+    f.read_to_end(&mut bytes).unwrap();
+    println!("Bytes read {} {:?}", bytes.len(), bytes);
+
+    // TODO: parse it and make slices and put in arrays...
+    
+
+    Ok(())
+  }
+
 }
 
 fn main() {
 
   let ta = Tuple{
-    key: &[1,2,3],
-    data: &[3,42,65],
+    key: &[65,66,67],
+    data: &[68,69,70],
   };
 
   let tb = Tuple{
-    key: &[1,2,7],
-    data: &[3,42,67],
+    key: &[65,66,68],
+    data: &[70,71,72],
   };
 
   let mut b = Bucket{
@@ -43,6 +103,9 @@ fn main() {
 
   b.insert(ta);
   b.insert(tb);
+
+  b.read().unwrap();
+  b.write().unwrap();
 
   println!("---Storage");
   println!("{:?}", b);
